80e442c43308e43b409be86843309ae7dfaf2de3
SonicSaurav <sunny45tk@gmail.com>
2023-10-12 01:56:55 +0530

    Var Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountid = 144553 //Cant Be Changed
+let accountemail = "sunny45tk@gmail.com" //Can Be Changed
+var accountpasssword = "sunny45tk" //Can Be Changed But Try Not Used Generally cause of scope issues and functional block isues
+accountcity = "Gaya" //Can Be Changed but try not to use this 
+let accountstate //Undefinded By Default
+
+
+
