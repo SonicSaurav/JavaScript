75c32e55d9919ad6a2692aa35644ad4ee40e76bc
SonicSaurav <sunny45tk@gmail.com>
2023-10-12 02:13:17 +0530

    Datatypes Done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name = "Sonic" //String
+let age = 22 //Number
+let isLoggedIn = false //Boolean
+
+//primitive Data Types
+//string => ""
+//boolean => True/False
+//bigint
+//number
+//undefined =>Value Not Defined
+//symbol => unique
+
+//Non Primitive Data Type
+//object
+//null => standalone value also empty categorized as object
+
+console.log(typeof undefined);
+
