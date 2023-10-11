20cf085199a4df2b185e80a19b70882518011459
SonicSaurav <sunny45tk@gmail.com>
2023-10-12 02:26:11 +0530

    Conversion Done

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+console.log(typeof score);
+let valueinnum = Number(score)
+console.log(typeof valueinnum);
+console.log(valueinnum);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// undefined => NaN
+// Null => 0
+
+let isloggedin = ""
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//Empty String or 0 => False
+//All except those True
