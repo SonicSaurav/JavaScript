53f8aeb7931837648c813b7bc2cc04196352ee06
SonicSaurav <sunny45tk@gmail.com>
2023-10-12 10:44:58 +0530

    Operations Done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue+value);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);//Exponenets
+console.log(2/3);
+console.log(2%3);//Remainder
+let str1 = "hello"
+let str2 = "Sonic"
+console.log(str1,str2);
+console.log("3"+3);
+console.log("3"+"3");
+//Dont Write this type of code 
+console.log(+true);
+console.log(+"");
+let num1 , num2 , num3 
+num1=num2=num3 = 2+2
+console.log(num1,num2,num3);
+//You can do this but dont do this
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+//Prefix and Postfrix Value increment before and after operation
