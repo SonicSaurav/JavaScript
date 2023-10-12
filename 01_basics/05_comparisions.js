a32737e49c546bec33725007962be7b2f327af47
SonicSaurav <sunny45tk@gmail.com>
2023-10-12 10:55:33 +0530

    Conversion done

---
+++ 01_basics/05_comparisions.js
@@ -0,0 +1,11 @@
+console.log(2>"1");
+//try to keep datatype same
+console.log(null >0);
+console.log(null==0);
+console.log(null >=0);
+//The Equality check and comparisions work different Comparision convert null to a number, treating it as 0 
+console.log(undefined == 0 );
+console.log(undefined <= 0);
+
+// Strict check === means it will also check for datatypes
+console.log(2==="2");
