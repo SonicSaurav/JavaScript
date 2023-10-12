c410b55b8babc1df46d57e2c4e1631e04abbacd5
SonicSaurav <sunny45tk@gmail.com>
2023-10-12 14:36:48 +0530

    Strings Done

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,18 @@
+let name = "Saurav"
+let repocount = 1
+console.log(`My name is ${name} and my repocount is ${repocount}`);//String interpolation
+const gamename = new String("Football")
+console.log(gamename);
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.charAt(1));
+console.log(gamename.toUpperCase());
+console.log(gamename.indexOf('b'));
+console.log(gamename.substring(0,4));
+const newstring = '   Sonic   '
+console.log(newstring.trim());
+console.log(newstring.replace("c","saurav"));
+console.log(gamename.includes("F"));
+let valname = "sonic-saurav-Joddd"
+console.log(valname.split('-'));
+console.log(valname[0]);
